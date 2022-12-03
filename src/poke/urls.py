18bf0ce98ce841476b80d87215a941a454e22afa
anoiraultepsi <119336938+anoiraultepsi@users.noreply.github.com>
2022-12-03 11:41:03 +0100

    Ajout de l'img, color et habitat sur /pokemon/nb

--- src/poke/urls.py
+++ src/poke/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name="index"),
-    path('hello',views.hello,name="hello"),
+    path('',views.home,name="home"),
     path('pokemon/<int:number>/',views.pokemon,name="pokemon"),
 ]
--- src/poke/views.py
+++ src/poke/views.py
@@ -4,22 +4,22 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(requests):
+def home(requests):
     context = {'name' : 'TITI', 'listPoke': ['Pikachu', 'Dracafau', 'Salamèche']}
-    return render(requests, 'pokeapp/index.html', context)
+    return render(requests, 'pokeapp/home.html', context)
 
-def hello(requests):
-    text = '<p> Liste des pokemons en cours de chargement</p> \
-    <div>Lorem ipsum... </div>'
-    return HttpResponse(text)
-
-def details(requeste, number):
-    text = '<h1> voici pokemon n°%d<h1>' % number
-    return HttpResponse(text)
 
 def pokemon(requests, number):
-    r = req.get("https://pokeapi.co/api/v2/pokemon/"+str(number))
-    result = r.json()
+    rname = req.get("https://pokeapi.co/api/v2/pokemon/"+str(number))
+    result = rname.json()
     name = result['name']
-    context = {'name': name}        
-    return render(requests,'pokeapp/index.html',context)
+    image = result['sprites']['front_default']
+
+    rcolor = req.get("https://pokeapi.co/api/v2/pokemon-species/"+str(number))
+    result = rcolor.json()
+
+    color = result['color']['name']
+    habitat = result['habitat']['name']
+
+    context = {'name': name, 'color' : color, 'habitat' : habitat , 'image' : image}        
+    return render(requests,'pokeapp/pokemon.html',context)
