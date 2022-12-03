a07420dc51bdb0754471d8214e2e55e14f75cb7e
SimonMEPSI <simon.martin4@epsi.fr>
2022-12-03 14:16:00 +0100

    Création du pokédex

--- src/poke/views.py
+++ src/poke/views.py
@@ -13,6 +13,7 @@ def pokemon(requests, number):
     r = req.get("https://pokeapi.co/api/v2/pokemon/"+str(number))
     result = r.json()
     
+    weight = result['weight']
     image = result['sprites']['other']['home']['front_default']
 
     rinfo = req.get("https://pokeapi.co/api/v2/pokemon-species/"+str(number))
@@ -23,5 +24,5 @@ def pokemon(requests, number):
     color = result['color']['name']
     habitat = result['habitat']['name']
 
-    context = {'name': name, 'color' : color, 'habitat' : habitat , 'image' : image}        
+    context = {'name': name, 'color' : color, 'habitat' : habitat , 'weight' : weight , 'image' : image }        
     return render(requests,'pokeapp/pokemon.html',context)
